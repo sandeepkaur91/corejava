17ecc9bccb5d4a221a6459c8f19cc34d04560152
sandeepchhina <sandeepchhina934@gmail.com>
2016-08-25 17:17:45 +0530

    format correct

--- src/org/java/socketprogramming/Urlreader.java
+++ src/org/java/socketprogramming/Urlreader.java
@@ -7,15 +7,15 @@ import java.net.URL;
 
 public class Urlreader {
 
-	public static void main(String[] args) throws Exception{
-		URL aUrl=new URL("http://luv2trip.com");
-BufferedReader sande=new BufferedReader(new InputStreamReader( aUrl.openStream()));
-String inputline;
-while((inputline =sande.readLine())!=null){
-	System.out.println(inputline);
-	
-}
-	sande.close();
+	public static void main(String[] args) throws Exception {
+		URL aUrl = new URL("http://luv2trip.com");
+		BufferedReader sande = new BufferedReader(new InputStreamReader(aUrl.openStream()));
+		String inputline;
+		while ((inputline = sande.readLine()) != null) {
+			System.out.println(inputline);
+
+		}
+		sande.close();
 	}
 
 }
